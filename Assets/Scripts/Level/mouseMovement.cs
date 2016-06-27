60b202b016362b57baf585849b236f72b8b0720a
Alfonso Nava Morales <alfonsonavamorales@gmail.com>
2016-06-27 22:27:57 +0200

    Editor v0.03

--- Assets/Scripts/Level/mouseMovement.cs
+++ Assets/Scripts/Level/mouseMovement.cs
@@ -14,16 +14,31 @@ public class mouseMovement : MonoBehaviour {
 	void Update () {
 		RaycastHit hit;
 		Ray touchRay = new Ray (transform.position, Vector3.forward);
-		int rayDistance = 100;
+		int rayDistance = 20;
 		Debug.DrawRay (transform.position, Vector3.forward * rayDistance);
-		if (Input.GetMouseButtonDown(0)) {
+		if (Input.GetMouseButton(0)) {
 			if (Physics.Raycast (touchRay, out hit, rayDistance)) {
 				if (hit.transform.tag == "planosEditor") {
-					hit.transform.GetComponent<MeshRenderer> ().material = newMaterial;
+					//hit.transform.GetComponent<MeshRenderer> ().material = newMaterial;
+					hit.transform.GetComponent<MeshRenderer> ().material.SetTexture("_MainTex", newTexture);
 				}
 			}
 		}
 		posMouse = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		transform.position = new Vector3(posMouse.x,posMouse.y,transform.position.z);
+	}
+
+	public Texture btnDirtTexture;
+	public Texture btnRockTexture;
+	public Texture btnDotsTexture;
+	public Texture newTexture;
+
+	void OnGUI() {		
+		if (GUI.Button (new Rect (Screen.width - 110, 10, 100, 100), btnDirtTexture))
+			newTexture = btnDirtTexture;
+		if (GUI.Button (new Rect (Screen.width - 110, 120, 100, 100), btnRockTexture))
+			newTexture = btnRockTexture;
+		if (GUI.Button (new Rect (Screen.width - 110, 230, 100, 100),btnDotsTexture))
+			newTexture = btnDotsTexture;
 	}
 }
