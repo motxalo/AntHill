53e0e01e5baed62f345add86f5e80f6515aa2bbd
Dani <daniandia@gmail.com>
2016-06-27 14:05:47 +0200

    mas nu guarece

---
+++ Assets/Scripts/Menus/GameModeMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameModeMenu : MonoBehaviour {
+
+	public void Start1PGame(){
+		PlayerPrefs.SetInt("Players",1);
+		Application.LoadLevel(1);
+	}
+
+	public void Start2PGame(){
+		PlayerPrefs.SetInt("Players",2);
+		Application.LoadLevel(1);
+	}
+}
