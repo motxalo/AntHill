dab2b5bdb9f1e54e1e2f1a22aa50186fa917befb
Dani <daniandia@gmail.com>
2016-07-12 16:52:52 +0200

    berserker joins the party

--- Assets/Scripts/Player/Characters/CharBerzerk.cs
+++ Assets/Scripts/Player/Characters/CharBerzerk.cs
@@ -5,17 +5,37 @@ public class CharBerzerk : MonoBehaviour {
 
 	private PlayerController controller;
 
+	public float timeToRestore = 5f;
+
 	// Use this for initialization
 	void Start () {
 		controller = GetComponent<PlayerController>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
+
+	float hitDistance = 1f;
 
 	public void DoSpecialAttack(){
 		controller.canSpecial = false;
+		Invoke("RestoreSpecialAttack",timeToRestore);
+		CuerpoACuerpoMuerte();
+	}
+
+	public void RestoreSpecialAttack(){
+		controller.canSpecial = true;
+	}	
+
+	void CuerpoACuerpoMuerte(){
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position + new Vector3(0f,.1f,0f) + transform.forward*.4f, transform.forward,out hit, hitDistance)){
+			if(hit.collider.tag == "Player"){
+				hit.collider.SendMessage("Die");
+			}
+			Debug.Log("ATAQUE BERSERKER A : "+hit.collider.name);
+		}
 	}
 }
