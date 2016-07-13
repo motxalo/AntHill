453dcad7d0c26ad39650ff50d40364ded92bf626
Dani <daniandia@gmail.com>
2016-07-13 16:46:38 +0200

    kill death scorer

--- Assets/Scripts/Menus/InGameMenu.cs
+++ Assets/Scripts/Menus/InGameMenu.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 public class InGameMenu : MonoBehaviour {
 
+	public struct StatLine{
+		public UnityEngine.UI.Text stName;
+		public UnityEngine.UI.Text stDeaths;
+		public UnityEngine.UI.Text stKills;
+	}
+
+	public StatLine[] statLines;
+
 	// Use this for initialization
 	void Start () {
+		CreateScorer();
 		HideMenu();
 	}
 	
@@ -15,6 +24,7 @@ public class InGameMenu : MonoBehaviour {
 	}
 
 	public void ShowMenu(){
+		ShowStats();
 		GetComponent<Canvas>().enabled = true;
 		Time.timeScale = 0f;
 	}
@@ -26,5 +36,33 @@ public class InGameMenu : MonoBehaviour {
 
 	public void ExitToMenu(){
 		Application.LoadLevel(0);
+	}
+
+	void CreateScorer(){
+		StatLine startingStatLine = new StatLine();
+		startingStatLine.stName 	= transform.GetChild(3).GetComponent<UnityEngine.UI.Text>();
+		startingStatLine.stKills 	= transform.GetChild(4).GetComponent<UnityEngine.UI.Text>();
+		startingStatLine.stDeaths	= transform.GetChild(5).GetComponent<UnityEngine.UI.Text>();
+		int playersAmount = PlayerPrefs.GetInt("Players");
+		statLines = new StatLine[playersAmount];
+		for (int i=0; i<playersAmount; i++){
+			statLines[i].stName = Instantiate(startingStatLine.stName) as UnityEngine.UI.Text;
+			statLines[i].stKills = Instantiate(startingStatLine.stKills) as UnityEngine.UI.Text;
+			statLines[i].stDeaths = Instantiate(startingStatLine.stDeaths) as UnityEngine.UI.Text;
+			statLines[i].stName.transform.parent = transform;
+			statLines[i].stDeaths.transform.parent = transform;
+			statLines[i].stKills.transform.parent = transform;
+			statLines[i].stName.rectTransform.position   = startingStatLine.stName.rectTransform.position - new Vector3(0f, 50f*(i+1), 0f);
+			statLines[i].stKills.rectTransform.position  = startingStatLine.stKills.rectTransform.position - new Vector3(0f, 50f*(i+1), 0f);
+			statLines[i].stDeaths.rectTransform.position = startingStatLine.stDeaths.rectTransform.position - new Vector3(0f, 50f*(i+1), 0f);
+		}
+	}
+
+	void ShowStats(){
+		for (int i=0; i<statLines.Length; i++){
+			statLines[i].stName.text = "Player "+(i+1);
+			statLines[i].stKills.text = ""+StatManager.points[i];
+			statLines[i].stDeaths.text = ""+StatManager.deaths[i];
+		}
 	}
 }
