226c170088ef2207051f1f0599072696267f11df
Alfonso Nava Morales <alfonsonavamorales@gmail.com>
2016-07-25 16:43:38 +0200

    enemy path en bragas

--- Assets/Scripts/LevelEditor/mouseMovement.cs
+++ Assets/Scripts/LevelEditor/mouseMovement.cs
@@ -4,10 +4,16 @@ using System.Collections;
 public class mouseMovement : MonoBehaviour {
 	Vector3 posMouse;
 	public Material newMaterial;
+	public bool pathMode;
+	public Texture pathTexture;
+	public Texture2D normalCursor;
+	public Texture2D blockedCursor;
+	public int enemyX;
+	public int enemyY;
 
 	// Use this for initialization
 	void Start () {
-	
+		Cursor.SetCursor(normalCursor,new Vector2(0,0),CursorMode.Auto);
 	}
 
     // Update is called once per frame
@@ -17,45 +23,63 @@ public class mouseMovement : MonoBehaviour {
         int rayDistance = 20;
         Debug.DrawRay(transform.position, Vector3.forward * rayDistance);
 
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            if (Input.GetMouseButton(0)) {
-                if (Physics.Raycast(touchRay, out hit, rayDistance)) {
-                    if (hit.transform.tag == "planosEditor") {
-                        if (selectedTileId < 0) return;
-                        hit.transform.GetComponent<MeshRenderer>().material.SetTexture("_MainTex", selectedTileTexture);
-                        // RELLENO LA LISTA DE TILES
-                        string[] cellname = hit.transform.name.Split('_');
-                        LevelEditorManager.SetTile(int.Parse(cellname[1]), int.Parse(cellname[2]), selectedTileId);
-                    }
-                }
-            }
-        }
-        else
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                if (Physics.Raycast(touchRay, out hit, rayDistance))
-                {
-                    if (hit.transform.tag == "planosEditor")
-                    {
-                        if (selectedTileId < 0) return;
-                        hit.transform.GetComponent<MeshRenderer>().material.SetTexture("_MainTex", selectedTileTexture);
-                        // RELLENO LA LISTA DE TILES
-                        string[] cellname = hit.transform.name.Split('_');
-                        LevelEditorManager.SetTile(int.Parse(cellname[1]), int.Parse(cellname[2]), selectedTileId);
-                    }
-                    else if (hit.transform.tag == "botonEditor")
-                    {
-                        hit.transform.GetComponent<LevelEditorButton>().DoClicked();
-                    }
-                    else if (hit.transform.tag == "tabEditor")
-                    {
-                        hit.transform.GetComponent<LevelEditorTab>().DoClicked();
-                    }
-                }
-            }
-        }
+		if (pathMode) {
+			if (Physics.Raycast (touchRay, out hit, rayDistance)) {
+				if (hit.transform.tag == "planosEditor") {
+					string[] cellname = hit.transform.name.Split ('_');
+					LevelEditorManager.SetTile (int.Parse (cellname [1]), int.Parse (cellname [2]), selectedTileId);
+					if (int.Parse (cellname [1]) == enemyX || int.Parse (cellname [2]) == enemyY) {
+						Cursor.SetCursor(normalCursor,new Vector2(0,0),CursorMode.Auto);
+						if (Input.GetMouseButton (0)) {
+							//aqui habria que guardar el tile, y tintarlo o algo para que se vea
+							enemyX = int.Parse (cellname [1]);
+							enemyY = int.Parse (cellname [2]);
+						}
+					}
+					else
+						Cursor.SetCursor(blockedCursor,new Vector2(0,0),CursorMode.Auto);
+				}
+			}
+		} else {
+
+			if (Input.GetKey (KeyCode.LeftShift)) {
+				if (Input.GetMouseButton (0)) {
+					if (Physics.Raycast (touchRay, out hit, rayDistance)) {
+						if (hit.transform.tag == "planosEditor") {
+							if (selectedTileId < 0)
+								return;
+							hit.transform.GetComponent<MeshRenderer> ().material.SetTexture ("_MainTex", selectedTileTexture);
+							// RELLENO LA LISTA DE TILES
+							string[] cellname = hit.transform.name.Split ('_');
+							LevelEditorManager.SetTile (int.Parse (cellname [1]), int.Parse (cellname [2]), selectedTileId);
+						}
+					}
+				}
+			} else {
+				if (Input.GetMouseButtonDown (0)) {
+					if (Physics.Raycast (touchRay, out hit, rayDistance)) {
+						if (hit.transform.tag == "planosEditor") {
+							if (selectedTileId < 0)
+								return;
+							hit.transform.GetComponent<MeshRenderer> ().material.SetTexture ("_MainTex", selectedTileTexture);
+							// RELLENO LA LISTA DE TILES
+							string[] cellname = hit.transform.name.Split ('_');
+							LevelEditorManager.SetTile (int.Parse (cellname [1]), int.Parse (cellname [2]), selectedTileId);
+							Debug.Log (selectedTileId);
+							if (selectedTileId > 300) {
+								pathMode = true;
+								enemyX = int.Parse(cellname [1]);
+								enemyY = int.Parse(cellname [2]);
+							}
+						} else if (hit.transform.tag == "botonEditor") {
+							hit.transform.GetComponent<LevelEditorButton> ().DoClicked ();
+						} else if (hit.transform.tag == "tabEditor") {
+							hit.transform.GetComponent<LevelEditorTab> ().DoClicked ();
+						}
+					}
+				}
+			}
+		}
 		posMouse = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		transform.position = new Vector3(posMouse.x,posMouse.y,transform.position.z);
 	}
